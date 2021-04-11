670a1f3f391b09b3ba3f4cb5e95c2d9ab7cca1eb
raeganfaith <57030395+raeganfaith@users.noreply.github.com>
2021-04-11 09:25:38 +0800

    last update

--- Login Interface/src/FrameLogin.java
+++ Login Interface/src/FrameLogin.java
@@ -45,7 +45,7 @@ public class FrameLogin extends JFrame {
 	
 	
 	//Launch the application.
-	public static void main(String[] args) {
+	public static void main(String[] args) {    
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
